e482aec6cfaabc88e8ad9584cf3274dbfd071241
Jake Litwicki <jake.litwicki@gmail.com>
2017-03-28 21:45:47 -0700

    update command line

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 #
 # Setup your workstation for Tavro and provision a standalone dev environment.
 #
-#  git clone git@github.com:zoadilack/tavro-dev.git && bash tavro-dev/setup.sh
+#  git clone git@github.com:zoadilack/tavro-dev.git && cd tavro-dev && bash setup.sh
 #
 
 # Setup your workstation
-git clone git@github.com:zoadilack/scripts.git /usr/local/zoadilack-scripts && bash /usr/local/zoadilack-scripts/osx-dev.sh
+git clone git@github.com:zoadilack/scripts.git zoadilack-scripts && bash zoadilack-scripts/osx-dev.sh
 
 # Update all the latest submodules
 git pull && git submodule init && git submodule update && git submodule status
@@ -14,6 +14,9 @@ git pull && git submodule init && git submodule update && git submodule status
 # Copy the tavro provision config variables
 cp provisioning/config/env.yml.dist provisioning/config/vagrant.yml
 cp provisioning/config/parameters.yml.dist provisioning/config/parameters.yml
+
+# Install all the dependencies for the app
+app/yarn install
 
 # Run vagrant
 vagrant up
