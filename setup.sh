4db9b4b951002262c134e722991f62e0e7a130e3
Jake Litwicki <jake.litwicki@gmail.com>
2017-06-27 15:37:58 -0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -5,16 +5,22 @@
 #  git clone git@github.com:zoadilack/tavro-dev.git && cd tavro-dev && bash setup.sh
 #
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Setup your workstation
-git clone git@github.com:zoadilack/scripts.git zoadilack-scripts && bash zoadilack-scripts/osx-dev.sh
+#git clone git@github.com:zoadilack/scripts.git zoadilack-scripts && bash zoadilack-scripts/osx-dev.sh
 
 # Update all the latest submodules
-git pull && git submodule init && git submodule update && git submodule status
+#git pull && git submodule init && git submodule update && git submodule status
 
-cd api/api && composer --prefer-dist install && cd ../../
-cd admin/admin && composer install --prefer-dist && cd ../../
+print "Installing API"
+cd api/api && composer install && cd $DIR
 
-curl -O http://get.sensiolabs.org/sami.phar bin/sami
+#print "Installing ADMIN"
+#cd admin/admin && composer install --prefer-dist && cd $DIR
+
+wget -O http://get.sensiolabs.org/sami.phar
+mv sami.phar bin/sami
 chmod a+x bin/sami
 
 php bin/sami update api/docs/config.php -v
