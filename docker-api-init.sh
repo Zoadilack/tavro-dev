46a8a34db417196c504d0affca4d005032c8722b
Jake Litwicki <jake.litwicki@gmail.com>
2017-09-17 23:07:30 -0700

    config updates

--- docker-api-init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# ===============================================================
-# Setup an api environment by running essential Symfony commands
-# ===============================================================
-
-cd /var/www/html
-php bin/console doctrine:database:create
-php bin/console doctrine:migrations:migrate
