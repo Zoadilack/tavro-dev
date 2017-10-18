1481f4e49176419f5e81b4f88b69559b1a42223b
Jake Litwicki <jake.litwicki@gmail.com>
2017-10-17 23:39:51 -0700

    update init script

--- init.sh
+++ init.sh
@@ -22,6 +22,7 @@ git submodule foreach git pull origin master
 git submodule foreach git checkout master
 
 API_VENDOR_DIR="$DIR/api/api/vendor"
+
 if [ ! -d "$API_VENDOR_DIR" ]; then
   echo "Installing API"
   cd api/api && composer install -v --prefer-dist --no-plugins && cd $DIR
@@ -44,8 +45,34 @@ mv sami.phar bin/sami
 chmod a+x bin/sami
 
 echo "Building API Docs"
-php bin/sami update api/docs/config.php -v
+php -i memory_limit=-1 bin/sami update api/docs/config.php -v
 
-docker-machine create tavro
-docker run -d --name nginx -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+# Setup the local proxy stuff
+docker-machine status default 2> /dev/null || docker-machine create default
+eval $(docker-machine env default)
+
+brew ls --versions dnsmasq && brew upgrade dnsmasq || brew install dnsmasq
+sudo echo "address/dev/$(docker-machine ip)" >> /usr/local/etc/dnsmasq.conf
+sudo mkdir -p /etc/resolver
+echo 'nameserver 127.0.0.1' | sudo tee /etc/resolver/dev
+
+LINE="127.0.0.1  admin.tavro.dev app.tavro.dev api.tavro.dev docs.tavro.dev > /dev/null &"
+FILE=/etc/hosts
+
+sudo grep -q "api.tavro.dev" /etc/hosts || sudo -- sh -c -e "echo '127.0.0.1   api.tavro.dev' >> /etc/hosts";
+sudo grep -q "admin.tavro.dev" /etc/hosts || sudo -- sh -c -e "echo '127.0.0.1   admin.tavro.dev' >> /etc/hosts";
+sudo grep -q "app.tavro.dev" /etc/hosts || sudo -- sh -c -e "echo '127.0.0.1   app.tavro.dev' >> /etc/hosts";
+sudo grep -q "docs.tavro.dev" /etc/hosts || sudo -- sh -c -e "echo '127.0.0.1   docs.tavro.dev' >> /etc/hosts";
+sudo grep -q "db.tavro.dev" /etc/hosts || sudo -- sh -c -e "echo '127.0.0.1   db.tavro.dev' >> /etc/hosts";
+sudo grep -q "phpmyadmin.tavro.dev" /etc/hosts || sudo -- sh -c -e "echo '127.0.0.1   phpmyadmin.tavro.dev' >> /etc/hosts";
+sudo grep -q "elk.tavro.dev" /etc/hosts || sudo -- sh -c -e "echo '127.0.0.1   elk.tavro.dev' >> /etc/hosts";
+
+sudo brew services restart dnsmasq
+
+# Create nginx-proxy container if it doesn't exist
+[ ! "$(docker network ls | grep nginx-proxy)" ] && docker network create nginx-proxy
+
+# Load up all the containers
 docker-compose up -d --build
+
+dscacheutil -flushcache
